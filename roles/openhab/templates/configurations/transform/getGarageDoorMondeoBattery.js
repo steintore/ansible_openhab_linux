0cd5d389d29ca803e7b0c1cfb9a4118861564d4c
Stein Tore Tøsse <stein.tore@kodemaker.no>
2015-06-08 07:33:26 +0200

    Added configurations and extracted sensistive info

---
+++ roles/openhab/templates/configurations/transform/getGarageDoorMondeoBattery.js
@@ -0,0 +1,6 @@
+var returnValue = 0
+JSON.parse(input).data.devices.forEach(function(element, index, array) {
+    if (element.id == 'ZWayVDev_zway_8-0-128') {
+        returnValue = element.metrics.level;
+    }});
+returnValue;
