794010d13344266eb3442625c32eab48108303c3
Stein Tore Tøsse <stein.tore@kodemaker.no>
2015-08-22 16:02:46 +0200

    Get it working

--- roles/openhab/templates/configurations/transform/getBasicOnOffState.js
+++ roles/openhab/templates/configurations/transform/getBasicOnOffState.js
@@ -1,5 +1,6 @@
 var returnValue = 'OFF'
 var value = JSON.parse(input).data.level.value
 
-if (value == 255) returnValue = 'ON'
+if (value == 0 || value == 'false') returnValue == 'OFF'
+else if (value == 255 || value == 'true' || value == true) returnValue = 'ON'
 returnValue;
