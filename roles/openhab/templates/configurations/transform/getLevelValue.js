78216b017915e6754025380481fd5e2770cc4b5d
Stein Tore Tøsse <stein.tore@kodemaker.no>
2015-08-22 18:28:33 +0200

    Some fixes

--- roles/openhab/templates/configurations/transform/getLevelValue.js
+++ roles/openhab/templates/configurations/transform/getLevelValue.js
@@ -1,1 +1,7 @@
-JSON.parse(input).level.value
+var value = JSON.parse(input).level.value
+var returnValue = 'OFF'
+
+if (returnValue == 'false' || returnValue == 0 || !value) returnValue = 'OFF'
+else returnValue = 'ON'
+
+returnValue
